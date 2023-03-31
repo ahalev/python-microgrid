e8784db67604343f5377f396547196c862d71b14
ahalev <avishaihalev@gmail.com>
2023-03-30 17:49:30 -0700

    add dry_run docstring

--- src/pymgrid/utils/dry_run.py
+++ src/pymgrid/utils/dry_run.py
@@ -8,6 +8,31 @@ import pymgrid
 
 @contextmanager
 def dry_run(pymgrid_object: Union['pymgrid.Microgrid', 'pymgrid.modules.base.BaseMicrogridModule']):
+    """
+    A context manager to test modifications of a pymgrid object without modifying said object.
+
+    Parameters
+    ----------
+    pymgrid_object : :class:`pymgrid.Microgrid` or :class:`pymgrid.modules.base.BaseMicrogridModule`
+        Microgrid or module to try run
+
+    Returns
+    -------
+    test_object : copy of object to test usage of, without modifying original object.
+
+    Examples
+    --------
+    >>> from pymgrid.modules import BatteryModule
+    >>> module = BatteryModule(0, 100, 50, 50, 0.9, init_soc=0.5)
+    >>> with dry_run(module) as test_module:
+    >>>     test_module.step(test_module.max_act, normalized=False)
+    >>>     print(f'Current step: {test_module.current_step}; current charge: {test_module.current_charge}')
+    Current step: 1; current charge: 0.0
+    >>> print(f'Current step: {module.current_step}; current charge: {module.current_charge}')
+    Current step: 0; current charge: 50.0
+
+    """
+
     serialized = yaml.safe_dump(pymgrid_object)
 
     try:
