ac53d7b2820dbf5264e2895ce3df1b21c3106dbb
ahalev <avishaihalev@gmail.com>
2023-04-19 13:28:30 -0700

    add tests for different normalized bounds

--- tests/microgrid/space/test_module_space.py
+++ tests/microgrid/space/test_module_space.py
@@ -46,3 +46,32 @@ class TestModuleSpace(TestCase):
                 denormalized = space.denormalize(val_to_denormalize)
                 self.assertEqual(denormalized, expected_denormalized_val)
 
+    def test_normalize_different_normalized_bounds(self):
+        unnorm_low = np.zeros(2)
+        unnorm_high = 2 * np.arange(2)
+        normalized_bounds = [-3, 2]
+
+        space = self.get_space(unnorm_low, unnorm_high, normalized_bounds=normalized_bounds)
+
+        vals_to_normalize = [np.zeros(2), np.array([0.5, 1]), np.array([1, 2])]
+        expected_normalized_vals = [np.array([-3, -3]), np.array([-0.5, -0.5]), np.array([2, 2])]
+
+        for val_to_normalize, expected_normalized_val in zip(vals_to_normalize, expected_normalized_vals):
+            with self.subTest(val_to_normalize=val_to_normalize, expected_normalized_val=expected_normalized_val):
+                normalized = space.normalize(val_to_normalize)
+                self.assertEqual(normalized, expected_normalized_val)
+
+    def test_denormalize_different_normalized_bounds(self):
+        unnorm_low = np.zeros(2)
+        unnorm_high = 2 * np.arange(2)
+        normalized_bounds = [-3, 2]
+
+        space = self.get_space(unnorm_low, unnorm_high, normalized_bounds=normalized_bounds)
+
+        vals_to_denormalize = [np.array([-3, -3]), np.array([-0.5, -0.5]), np.array([2, 2])]
+        expected_denormalized_vals = [np.zeros(2), np.array([0.5, 1]), np.array([1, 2])]
+
+        for val_to_denormalize, expected_denormalized_val in zip(vals_to_denormalize, expected_denormalized_vals):
+            with self.subTest(val_to_denormalize=val_to_denormalize, expected_denormalized_val=expected_denormalized_val):
+                denormalized = space.denormalize(val_to_denormalize)
+                self.assertEqual(denormalized, expected_denormalized_val)
