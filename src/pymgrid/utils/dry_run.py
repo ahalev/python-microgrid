15c8f7abe47ca45004dbf5a42c23966079c07bff
ahalev <avishaihalev@gmail.com>
2023-04-27 16:13:24 -0700

    make dry run just a deepcopy

--- src/pymgrid/utils/dry_run.py
+++ src/pymgrid/utils/dry_run.py
@@ -1,5 +1,4 @@
-import yaml
-
+from copy import deepcopy
 from contextlib import contextmanager
 from typing import Union
 
@@ -8,18 +7,4 @@ import pymgrid
 
 @contextmanager
 def dry_run(pymgrid_object: Union['pymgrid.Microgrid', 'pymgrid.modules.base.BaseMicrogridModule']):
-    serialized = yaml.safe_dump(pymgrid_object)
-
-    try:
-        yield pymgrid_object
-    finally:
-        deserialized = yaml.safe_load(serialized)
-        try:
-            # Module
-            data = deserialized._serialize_state_attributes()
-        except AttributeError:
-            # Microgrid
-            data = deserialized._serialization_data()
-            pymgrid_object._modules = deserialized.modules
-
-        pymgrid_object.deserialize(data)
+    yield deepcopy(pymgrid_object)
