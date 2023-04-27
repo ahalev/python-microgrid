c61bff44b6f93087a94045abc21198f6f7575cda
ahalev <avishaihalev@gmail.com>
2023-04-26 21:08:55 -0700

    update assertNotEqual

--- tests/helpers/test_case.py
+++ tests/helpers/test_case.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 import pandas as pd
 
+from unittest.util import safe_repr
 
 class TestCase(unittest.TestCase):
     def assertEqual(self, first, second, msg=None) -> None:
@@ -20,3 +21,16 @@ class TestCase(unittest.TestCase):
                     np.testing.assert_allclose(first, second, rtol=1e-7, atol=1e-10, err_msg=msg if msg else '')
                 except TypeError:
                     raise e
+
+    def assertNotEqual(self, first, second, msg=None) -> None:
+        try:
+            super().assertNotEqual(first, second, msg=msg)
+        except ValueError as e:
+            try:
+                self.assertEqual(first, second)
+            except AssertionError:
+                pass
+            else:
+                msg = self._formatMessage(msg, '%s == %s' % (safe_repr(first),
+                                                             safe_repr(second)))
+                raise self.failureException(msg)
