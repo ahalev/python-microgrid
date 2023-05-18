218692ac65dbf625dbde64780b3569da8e58de83
ahalev <avishaihalev@gmail.com>
2023-05-18 10:26:27 -0700

    add env

--- src/pymgrid/envs/continuous/continuous.py
+++ src/pymgrid/envs/continuous/continuous.py
@@ -44,3 +44,93 @@ class ContinuousMicrogridEnv(BaseMicrogridEnv):
 
         return flatten(self._nested_action_space, _action)
 
+
+class NetLoadContinuousMicrogridEnv(BaseMicrogridEnv):
+    """
+    A continuous action space environment where actions are percentage of the net load to be fulfilled by each module
+
+    E.g. action = [a_battery, a_grid]:
+    a_battery is % of net load to be fulfilled by battery,
+    a_grid is % to be fulfilled by grid
+        """
+    _nested_action_space = None
+    check_actions = True
+
+    def _get_action_space(self, remove_redundant_actions=False):
+        self._nested_action_space = self._get_nested_action_space
+        return flatten_space(self._nested_action_space) if self._flat_spaces else self._nested_action_space
+
+    @property
+    def _get_nested_action_space(self):
+        as_builtins_unnormalized = extract_builtins(self._modules.get_attrs('action_space', 'module_type', as_pandas=False), 'act')
+        as_builtins_normalized = Dict({
+            name: Tuple([Box(low=0, high=1, shape=action_space.shape) for action_space in as_list])
+            for name, as_list in as_builtins_unnormalized.items()
+        })
+
+        microgrid_space = MicrogridSpace(as_builtins_unnormalized, as_builtins_normalized)
+
+        return Dict({name: Tuple([Box(low=0, high=1, shape=module.action_space.shape) for module in modules_list])
+                     for name, modules_list in self.modules.controllable.iterdict()})
+
+    def compute_net_load(self):
+        """
+        Net load is load minus renewables.
+        TODO: make renewable a fixed module
+        Returns
+        -------
+
+        """
+        try:
+            fixed_consumption = self.modules.fixed.get_attrs('max_consumption').sum().item()
+        except AttributeError:
+            fixed_consumption = 0.0
+
+        try:
+            fixed_production = self.modules.fixed.get_attrs('max_production').sum().item()
+        except AttributeError:
+            fixed_production = 0.0
+
+        return fixed_consumption - fixed_production
+
+    def convert_action(self, action, to_microgrid=True):
+        # TODO test this. Actions are percentages
+        # TODO add a slack module. It should not be in action space and it should be controllable and it
+        # should be given an absolute action to balance the rest.
+
+        if to_microgrid:
+            relative_action = unflatten(self._nested_action_space, action)
+            absolute_action = self._get_absolute_action(relative_action)
+
+            self._check_action(absolute_action)
+
+            return absolute_action
+
+        relative_action = self._get_relative_action(action)
+
+        return flatten(self._nested_action_space, relative_action)
+
+    def _get_absolute_action(self, relative_action):
+        # TODO test this
+        net_load = self.compute_net_load()
+
+        return MicrogridSpace.dict_op(relative_action, net_load, operator.mul)
+
+    def _get_relative_action(self, absolute_action):
+        # TODO test this
+        net_load = self.compute_net_load()
+
+        return MicrogridSpace.dict_op(absolute_action, net_load, operator.truediv)
+
+    def _check_action(self, absolute_action):
+        if self.check_actions:
+            try:
+                assert absolute_action in self._nested_action_space
+            except AssertionError:
+                clipped = self.microgrid_action_space.clip(absolute_action)
+
+                if np.isclose(flatten(self._nested_action_space, absolute_action),
+                              flatten(self._nested_action_space, absolute_action)).all():
+                    _action = clipped
+                else:
+                    raise
