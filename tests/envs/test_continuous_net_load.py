28b84ac1d5ff8f2b50de3e04e6cb60970d6cea69
ahalev <avishaihalev@gmail.com>
2023-05-19 16:09:54 -0700

    add convert action tests

--- tests/envs/test_continuous_net_load.py
+++ tests/envs/test_continuous_net_load.py
@@ -1,5 +1,6 @@
+import numpy as np
+
 from copy import deepcopy
-from math import factorial
 
 from tests.helpers.test_case import TestCase
 from tests.helpers.modular_microgrid import get_modular_microgrid
@@ -36,12 +37,38 @@ class TestNetLoadContinuousEnv(TestCase):
         pass
 
     def test_convert_action_to_absolute(self):
-        # TODO write; check absolute
-        pass
+        microgrid = get_modular_microgrid()
+        env = NetLoadContinuousMicrogridEnv.from_microgrid(microgrid)
+
+        expected_absolute_action = {
+            'battery': [np.array([5])],
+            'genset': [np.array([0, 2.5])],
+            'grid': [np.array([2.5])]
+        }
+
+        relative_action = np.array([0.5, 0, 0.25, 0.25])
+
+        absolute_action = env.convert_action(relative_action)
+
+        for module_name, action_list in expected_absolute_action.items():
+            for module_num, act in enumerate(action_list):
+                with self.subTest(module_name=module_name, module_num=module_num):
+                    self.assertEqual(act, absolute_action[module_name][module_num])
 
     def test_convert_action_to_relative(self):
-        # TODO write; check relative
-        pass
+        microgrid = get_modular_microgrid()
+        env = NetLoadContinuousMicrogridEnv.from_microgrid(microgrid)
+
+        expected_relative_action = np.array([0.5, 0, 0.25, 0.25])
+
+        absolute_action = {
+            'battery': [np.array([5])],
+            'genset': [np.array([0, 2.5])],
+            'grid': [np.array([2.5])]
+        }
+
+        relative_action = env.convert_action(absolute_action, to_microgrid=False)
+        self.assertEqual(relative_action, expected_relative_action)
 
 
 class TestNetLoadContinuousEnvScenario(TestCase):
