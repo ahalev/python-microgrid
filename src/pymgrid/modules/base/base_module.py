38b2d1af6033817a072019091af61962f8718955
ahalev <avishaihalev@gmail.com>
2023-09-11 14:56:34 -0700

    do not copy dict in logging

--- src/pymgrid/modules/base/base_module.py
+++ src/pymgrid/modules/base/base_module.py
@@ -278,20 +278,19 @@ class BaseMicrogridModule(yaml.YAMLObject):
         return self.update(absorbed_energy, as_sink=True)
 
     def _log(self, state_dict_pre_step, provided_energy=None, absorbed_energy=None, **info):
-        _info = info.copy()
+        energy_info = dict()
 
         if self.provided_energy_name is not None:
-            _info[self.provided_energy_name] = provided_energy if provided_energy is not None else 0.0
+            energy_info[self.provided_energy_name] = provided_energy if provided_energy is not None else 0.0
         else:
             assert provided_energy is None, 'Cannot log provided_energy with NoneType provided_energy_name.'
 
         if self.absorbed_energy_name is not None:
-            _info[self.absorbed_energy_name] = absorbed_energy if absorbed_energy is not None else 0.0
+            energy_info[self.absorbed_energy_name] = absorbed_energy if absorbed_energy is not None else 0.0
         else:
             assert absorbed_energy is None, 'Cannot log absorbed_energy with NoneType absorbed_energy_name.'
 
-        _info.update(state_dict_pre_step)
-        self._logger.log(**_info)
+        self._logger.log(**info, **energy_info, **state_dict_pre_step)
 
     def _update_step(self, reset=False):
         if reset:
